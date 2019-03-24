980ca6cc207f5bbc4b6380583e603a5411bd0b8f
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-03-24 19:29:32 +0100

    more tests

---
+++ tests/test_basic_2.nim
@@ -0,0 +1,54 @@
+import closure_methods
+import strformat
+
+class(Person):
+  constructor:
+    proc newPerson(name: string, initAge: int)
+
+  let name = name
+  var age = initAge
+
+  proc getAge*(): int = age
+
+  proc greet*() =
+    echo &"Hi, I'm {name} and I'm {self.getAge()} years old."
+
+  proc getOlder*() =
+    age += 1
+
+block:
+  let p: Person = newPerson("John", 42)
+  p.greet()
+  p.getOlder()
+  p.greet()
+  p.getOlder()
+  p.greet()
+  # Hi, I'm John and I'm 42 years old.
+  # Hi, I'm John and I'm 43 years old.
+  # Hi, I'm John and I'm 44 years old.
+
+
+
+classOf(HackedPerson, Person):
+  constructor:
+    proc newHackedPerson(name: string, initAge: int)
+
+  base(name & " Cheating", initAge)
+
+  var age = initAge.float
+
+  proc getAge*(): int = age.int
+
+  proc getOlder*() =
+    age += 0.5
+
+block:
+  let p: Person = newHackedPerson("John", 42)
+  p.greet()
+  p.getOlder()
+  p.greet()
+  p.getOlder()
+  p.greet()
+  # Hi, I'm John Cheating and I'm 42 years old.
+  # Hi, I'm John Cheating and I'm 42 years old.
+  # Hi, I'm John Cheating and I'm 43 years old.
