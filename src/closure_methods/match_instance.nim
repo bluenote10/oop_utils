deacd3e18b497b552b3d421a94e1f2e1f8a58e9d
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-04-07 11:03:22 +0200

    cleanup instance matching

--- src/closure_methods/match_instance.nim
+++ src/closure_methods/match_instance.nim
@@ -16,45 +16,14 @@ proc makeTemplate(ident: string, typeName: string): NimNode =
     )
   )
 
-#macro caseInstance*(x: typed, branches: varargs[untyped]): untyped =
-macro caseInstance*(x: typed, branches: untyped): untyped =
-  result = newNimNode(nnkIfExpr) # newIfStmt()
-
-  dumpTree:
-    let val = if x of A:
-      template x(): untyped = A(x)
-    elif x of B:
-      template x(): untyped = B(x)
-
-  echo branches.treeRepr
-
-  for branch in branches:
-    echo branch.treeRepr
-    if branch.kind == nnkCall:
-      expectKind branch[0], nnkIdent
-      expectKind branch[1], nnkStmtList
-      let typeName = branch[0].strVal
-      let elifBranch = newNimNode(nnkElifBranch).add(
-        infix(x, "of", ident typeName),
-        newStmtList(
-          makeTemplate(x.strVal, typeName)
-        )
-      )
-      for child in branch[1]:
-        elifBranch[1].add(child)
-      result.add(elifBranch)
-    else:
-      error &"Node not allowed:\n{branch.treeRepr}", branch
-
-  echo result.repr
 
 macro matchInstance*(body: untyped): untyped =
-  echo body.treeRepr
+  ## Macro the allow simple case statement based instance matching.
   expectKind body[0], nnkCaseStmt
   let caseStmt = body[0]
   let caseIdent = caseStmt[0]
 
-  result = newNimNode(nnkIfExpr) # newIfStmt()
+  result = newNimNode(nnkIfExpr)
   for i in 1 ..< caseStmt.len:
     let branch = caseStmt[i]
     if branch.kind == nnkOfBranch:
