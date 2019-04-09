0ba28fdf8ecb7c0dafd4740ad15f0160fa0106d8
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-04-09 22:05:10 +0200

    added test to verify closure behavior

---
+++ tests/test_closure_correctness.nim
@@ -0,0 +1,29 @@
+import closure_methods
+
+class(Test):
+  ctor(newTest) proc(s: openarray[int])
+
+  var s = @s
+
+  proc replace*(newS: openarray[int]) =
+    s = @newS
+
+  proc len*(): int = s.len
+  proc get*(i: int): int = s[i]
+  proc getAll*(): seq[int] = s
+
+block:
+  let t = newTest([1, 2, 3])
+
+  echo t.getAll()
+  doAssert t.len() == 3
+  doAssert t.get(0) == 1
+  doAssert t.get(1) == 2
+  doAssert t.get(2) == 3
+
+  t.replace([4, 5])
+
+  echo t.getAll()
+  doAssert t.len() == 2
+  doAssert t.get(0) == 4
+  doAssert t.get(1) == 5
